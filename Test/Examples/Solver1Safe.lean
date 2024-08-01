650464558d545170f50617a6d11c41c954858255
Adrien Champion <adrien.champion@protonmail.com>
2024-08-01 18:15:16 +0200

    chore: test `getValue`/`getValues` (safe versions)

--- Test/Examples/Solver1Safe.lean
+++ Test/Examples/Solver1Safe.lean
@@ -4,6 +4,7 @@ namespace cvc5.Safe.Test
 
 def work : IO Unit := Smt.run! do
   Smt.setOption "produce-proofs" "true"
+  Smt.setOption "produce-models" "true"
 
   Smt.setLogic "QF_LIA"
 
@@ -16,30 +17,45 @@ def work : IO Unit := Smt.run! do
   let neq ← Smt.mkNot eq
   let ite ← Smt.mkIte b eq neq
 
+  println! "assert {ite}"
   Smt.assertFormula ite
+  println! "assert {eq}"
   Smt.assertFormula eq
 
+  println! "\nchecksat"
   Smt.checkSat
-    (onSat := println! "confirmed `sat` result")
+    (onSat := do
+      println! "- confirmed `sat` result"
+      let bVal ← Smt.getValue b
+      println! "  getValue b := {bVal}"
+      let intVals ← Smt.getValues #[n1, n2]
+      println! "  getValues #[n1, n2] := {intVals}"
+    )
 
+  println! "\n\nreset\n\n"
   Smt.resetAssertions
 
   let imp1 ← Smt.mkImplies b eq
   let not_b ← Smt.mkNot b
   let imp2 ← Smt.mkImplies not_b neq
 
+  println! "assert {imp1}"
   Smt.assertFormula imp1
+  println! "assert {imp2}"
   Smt.assertFormula imp2
+  println! "assert {eq}"
   Smt.assertFormula eq
+  println! "assert {not_b}"
   Smt.assertFormula not_b
 
+  println! "\nchecksat"
   Smt.checkSat
     (onUnsat := do
-      println! "confirmed `unsat` result"
+      println! "- confirmed `unsat` result"
       let proofs ← Smt.getProof
-      println! "proof:"
+      println! "  proof :="
       for p in proofs do
-        println! "- {p.getResult}"
+        println! "    {p.getResult}"
     )
 
 
